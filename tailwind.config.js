0be559968f5858187480794b12d13599e26a0257
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-01-25 16:07:00 -0700

    Add tailwind init files with updated theme

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	content: ['./src/**/*.{js,jsx}'],
+	theme: {
+		colors: {
+			white: '#fff',
+			black: '#000',
+			gray: {
+				100: '#ededed',
+				200: '#dcdcdc',
+				300: '#585858'
+			}
+		},
+		plugins: []
+	}
+}
