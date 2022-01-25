711f7387dfe227d725ce698030967298b4702798
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-01-25 16:22:42 -0700

    Add API outline

---
+++ src/api/assessmentAPI.js
@@ -0,0 +1,9 @@
+/* ==========================================================================
+  api | Assessment API | getStudents
+========================================================================== */
+/**
+* @method: GET
+* @url: https://api.hatchways.io/assessment/students
+* @description: Get all students
+* @return: Array of students
+*/
