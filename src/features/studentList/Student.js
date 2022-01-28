5e56508ac07f25dd9b38b7ddd5e14405ab88d06a
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-01-28 12:29:47 -0700

    Refactor StudentList into List and Student component

---
+++ src/features/studentList/Student.js
@@ -0,0 +1,20 @@
+export const Student = ({student, average}) => {
+	return (
+		<li className='flex flex-wrap py-2 px-4 border-y border-gray-200'>
+			<img className='rounded-full border-black border w-32 h-32' src={student.pic} alt={student.firstName} />
+			<div className='px-5'>
+				<h2 className='font-bold uppercase text-4xl '>
+					{student.firstName} {student.lastName}
+				</h2>
+				<div className='py-2 px-5'>
+					<p>Email: {student.email}</p>
+					<p>Company: {student.company}</p>
+					<p>Skill: {student.skill}</p>
+					<p className='flex'>
+						Average: <span> {average}</span> %
+					</p>
+				</div>
+			</div>
+		</li>
+	)
+}
