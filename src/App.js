d2dc2424c57fb38fdc257632c3c674b1edf73665
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-01-27 18:39:29 -0700

    Add JSX to match the wireframe

--- src/App.js
+++ src/App.js
@@ -1,3 +1,48 @@
+import {useGetStudentsQuery} from './services/studentsApi'
+
 export const App = () => {
-	return <h1>Phasebook</h1>
+	const {data, isLoading, isError} = useGetStudentsQuery()
+
+	if (isLoading) return <p>Loading...</p>
+	if (isError) return <p>Error :(</p>
+
+	const students = data.students
+
+	// TODO:Create a helper function folder
+
+	const getAverage = (grades) => {
+		const sum = grades.reduce((acc, grade) => {
+			grade = parseInt(grade)
+			acc += grade
+			return acc
+		}, 0)
+
+		const average = sum / grades.length
+		console.log('👾: getAverage -> average', average)
+		return <p>{average}</p>
+	}
+
+	// TODO: move JSX logic into its own component
+
+	return (
+		<div>
+			<h1>Students</h1>
+			<ul>
+				{students.map((student) => (
+					<li key={student.id}>
+						<img src={student.pic} alt={student.firstName} />
+						<div>
+							<h2>
+								{student.firstName} {student.lastName}
+							</h2>
+							<p>Email: {student.email}</p>
+							<p>Company: {student.company}</p>
+							<p>Skill: {student.skill}</p>
+							{getAverage(student.grades)}
+						</div>
+					</li>
+				))}
+			</ul>
+		</div>
+	)
 }
