b3ec0aa3ed0bd25e4742c6f488ae16ed919780c9
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-01-28 10:21:28 -0700

    Organize imports

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import './style.css'
-
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
 import {BrowserRouter as Router} from 'react-router-dom'
-
 import {App} from './App'
 import {store} from './app/store'
+import './style.css'
 
 ReactDOM.render(
 	<Provider store={store}>
