64fa0fe7c87acc122749ae7ff4e7012a7faef6d4
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-01-27 18:39:05 -0700

    Fix store to use correct redux toolkit syntax

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,19 @@
-import {configureStore} from '@reduxjs/toolkit'
+import {combineReducers, configureStore} from '@reduxjs/toolkit'
+import {setupListeners} from '@reduxjs/toolkit/query'
+import {studentsApi} from '../services/studentsApi'
+
+import studentListReducer from '../features/studentReducer/studentListSlice'
+
+const rootReducer = combineReducers({
+	studentList: studentListReducer
+})
 
 export const store = configureStore({
-	reducer: {}
+	reducer: {
+		reducer: rootReducer,
+		[studentsApi.reducerPath]: studentsApi.reducer
+	},
+	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(studentsApi.middleware)
 })
+
+setupListeners(store.dispatch)
