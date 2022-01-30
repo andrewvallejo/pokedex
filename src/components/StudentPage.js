06c2d5527065060f35571059d493e8186e82a336
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-01-30 13:43:44 -0700

    Add student page component for rendering

---
+++ src/components/StudentPage.js
@@ -0,0 +1,9 @@
+import {List} from '../features/studentList/List'
+
+export const StudentPage = () => {
+	return (
+		<main className='h-screen flex flex-col bg-gray-100 font-body justify-center items-center'>
+			<List />
+		</main>
+	)
+}
