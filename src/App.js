74a5141dd6fdea700c9b7a84597967c88e112021
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-01-31 13:50:05 -0700

    Add useEffect to load redux state on load

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import {useEffect} from 'react'
+import {useSelector} from 'react-redux'
+import {useDispatch} from 'react-redux'
 import {Routes} from 'react-router-dom'
 import {Route} from 'react-router-dom'
+import {fetchStudents} from './api/studentApi'
 import {StudentPage} from './components/StudentPage'
 
 export const App = () => {
+	const dispatch = useDispatch()
+	const state = useSelector((state) => state)
+
+	useEffect(
+		() => {
+			dispatch(fetchStudents())
+		},
+		[dispatch]
+	)
+
 	return (
 		<Routes>
 			<Route path='/' element={<StudentPage />} />
