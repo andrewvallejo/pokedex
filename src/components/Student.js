cdce1c6dc538bfea7745ed1a28376a78f3fa6f9b
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-01-31 13:50:47 -0700

    Move file

---
+++ src/components/Student.js
@@ -0,0 +1,20 @@
+export const Student = ({student, average}) => {
+	return (
+		<li className='flex flex-wrap py-2 px-4 '>
+			<img className='rounded-full border-black border w-32 h-32' src={student.pic} alt={student.firstName} />
+			<div className='px-5'>
+				<h2 className='font-bold uppercase text-4xl '>
+					{student.firstName} {student.lastName}
+				</h2>
+				<div className='py-2 px-5'>
+					<p>Email: {student.email}</p>
+					<p>Company: {student.company}</p>
+					<p>Skill: {student.skill}</p>
+					<div className='flex'>
+						Average:<span>{average}</span>%
+					</div>
+				</div>
+			</div>
+		</li>
+	)
+}
