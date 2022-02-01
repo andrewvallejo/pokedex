d92717703237852a7e20f03d64fe9a0618e8a9bf
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-02-01 12:54:04 -0700

    Add test score toggle and logic

--- src/components/Student.js
+++ src/components/Student.js
@@ -3,34 +3,48 @@
  * @elements: id, fullName, email, company, skill, average(of grades), test scores(grades), picture(pic)
  */
 
+import {useState} from 'react'
+
 export const Student = ({student}) => {
+	const [isShowTestScores, setShowTestScores] = useState(false)
+
 	const testScores = student.grades.map((grade, i) => {
 		const testNumber = i + 1
-		return (
-			<li
-				key={`${student.name}: test ${testNumber}`}
-				className='flex flex-row self-start justify-between w-2/5 -space-x-4'>
-				<p>Test {testNumber}</p>
+		const test = (
+			<li key={`${student.name}: test ${testNumber}`} className='flex flex-row self-start justify-between w-3/5'>
+				<p>Test {testNumber}:</p>
 				<p>{grade}%</p>
 			</li>
 		)
+		return <ul className='flex flex-row'>{test}</ul>
 	})
 
+	const handleToggle = () => {
+		setShowTestScores(!isShowTestScores)
+	}
+
 	return (
-		<li className='flex flex-wrap py-2 px-4 '>
+		<li className='flex py-2 px-4'>
 			<img className='rounded-full border-black border w-32 h-32' src={student.pic} alt={student.firstName} />
-			<div className='px-5'>
-				<h2 className='font-bold uppercase text-4xl '>{student.name}</h2>
+			<article className='px-5'>
+				<h2 className='font-bold uppercase text-4xl'>{student.name}</h2>
 				<div className='py-2 px-5'>
 					<p>Email: {student.email}</p>
 					<p>Company: {student.company}</p>
 					<p>Skill: {student.skill}</p>
 					<div className='flex'>
-						Average:<span>{student.average}</span>%
+						<p>Average: {student.average}%</p>
 					</div>
+					{isShowTestScores && testScores}
 				</div>
-				<ul className='py-2 px-5'>{testScores}</ul>
-			</div>
+			</article>
+			<article className='flex justify-end align-start  w-full relative'>
+				<button
+					className='text-8xl text-gray-200 self-start hover:text-black outline-none focus-within:text-black'
+					onClick={handleToggle}>
+					{isShowTestScores ? '-' : '+'}
+				</button>
+			</article>
 		</li>
 	)
 }
