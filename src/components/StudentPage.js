b5efba630936d37db7b69ecc984f5ef247589107
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-01-31 19:08:02 -0700

    Add all filter logic to page

--- src/components/StudentPage.js
+++ src/components/StudentPage.js
@@ -2,41 +2,49 @@
 * @description: A list of all of the students
 * @elements: profile picture, name, email, company, skills, grade average
 */
-// import {SearchBar} from '../features/search/SearchBar'
 
 import {useEffect, useState} from 'react'
 import {useSelector} from 'react-redux'
-import {Student} from '../components/Student'
-import {SearchBar} from '../features/search/SearchBar'
+import {List} from './List'
+import {SearchBar} from './SearchBar'
 
 export const StudentPage = () => {
-	const {loading, studentList: {students}} = useSelector((state) => state.reducer.students)
+	const [loadedStudents, setLoadedStudents] = useState([])
 	const [isLoaded, setLoaded] = useState(false)
+	const {loading, studentList: {students}} = useSelector((state) => state.reducer.students)
+
+	const {search: {searchTerm, searchField}} = useSelector((state) => state.reducer)
 
 	useEffect(
 		() => {
-			if (loading === 'idle' && students) {
+			if (students) {
 				setLoaded(true)
+				setLoadedStudents(students)
+			}
+			if (searchTerm) {
+				setLoadedStudents(
+					students.filter((student) => {
+						return student[searchField].toLowerCase().includes(searchTerm.toLowerCase())
+					})
+				)
 			}
 		},
-		[loading, students]
+		[loading, searchField, searchTerm, students]
 	)
 
 	return (
 		<main className='h-screen flex flex-col bg-gray-100 font-body justify-center items-center'>
 			<section className='bg-white min-w-10 w-4/5 h-3/4 rounded-lg shadow-md overflow-auto'>
 				<nav className='px-2 min-w-full'>
-					<SearchBar field='name' />
+					<SearchBar field='name' filter={setLoadedStudents} />
 				</nav>
-				<div className='flex flex-col justify-center items-center'>
-					{isLoaded ? (
-						students.map((student) => <Student key={student.id} student={student} />)
-					) : (
-						<div className='flex flex-col justify-center items-center'>
-							<h1 className='text-3xl font-bold'>Loading...</h1>
-						</div>
-					)}
-				</div>
+				{isLoaded ? (
+					<List students={loadedStudents} />
+				) : (
+					<div className='flex flex-col justify-center items-center'>
+						<h1 className='text-3xl font-bold'>Loading...</h1>
+					</div>
+				)}
 			</section>
 		</main>
 	)
