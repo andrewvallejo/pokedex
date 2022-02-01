d5f79823a3c9c5ba52c3fd156ebb9c3caaadb2a7
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-01-31 17:50:17 -0700

    Add data cleaner to remove logic from Student

--- src/api/studentApi.js
+++ src/api/studentApi.js
@@ -2,6 +2,26 @@ import {createAsyncThunk} from '@reduxjs/toolkit'
 
 export const fetchStudents = createAsyncThunk('students/fetchStudents', async () => {
 	const response = await fetch('https://api.hatchways.io/assessment/students')
-	const data = await response.json()
+	const data = await response.json().then((data) => {
+		cleanData(data)
+		return data
+	})
 	return data
 })
+
+const cleanData = ({students}) => {
+	const getAverage = (grades) => {
+		return (
+			grades.reduce((acc, grade) => {
+				acc += parseInt(grade)
+				return acc
+			}, 0) / grades.length
+		)
+	}
+
+	students.forEach((student) => {
+		student.name = `${student.firstName} ${student.lastName}`
+		student.average = getAverage(student.grades)
+		delete student.city
+	})
+}
