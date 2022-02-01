f8ab7a2e6d77b952197e6f6613e8ba83a91ca3ea
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-02-01 13:56:14 -0700

    Fix minor visual bugs

--- src/components/Student.js
+++ src/components/Student.js
@@ -10,13 +10,12 @@ export const Student = ({student}) => {
 
 	const testScores = student.grades.map((grade, i) => {
 		const testNumber = i + 1
-		const test = (
+		return (
 			<li key={`${student.name}: test ${testNumber}`} className='flex flex-row self-start justify-between w-3/5'>
 				<p>Test {testNumber}:</p>
 				<p>{grade}%</p>
 			</li>
 		)
-		return <ul className='flex flex-row'>{test}</ul>
 	})
 
 	const handleToggle = () => {
@@ -25,23 +24,19 @@ export const Student = ({student}) => {
 
 	return (
 		<li className='flex py-2 px-4'>
-			<img className='rounded-full border-black border w-32 h-32' src={student.pic} alt={student.firstName} />
-			<article className='px-5'>
+			<img className='rounded-full self-center border w-32 h-32' src={student.pic} alt={student.firstName} />
+			<article className='px-5 w-full'>
 				<h2 className='font-bold uppercase text-4xl'>{student.name}</h2>
 				<div className='py-2 px-5'>
 					<p>Email: {student.email}</p>
 					<p>Company: {student.company}</p>
 					<p>Skill: {student.skill}</p>
-					<div className='flex'>
-						<p>Average: {student.average}%</p>
-					</div>
-					{isShowTestScores && testScores}
+					<p className='flex'>Average: {student.average}%</p>
+					{isShowTestScores && <ul className='flex flex-col pt-2'>{testScores}</ul>}
 				</div>
 			</article>
-			<article className='flex justify-end align-start  w-full relative'>
-				<button
-					className='text-8xl text-gray-200 self-start hover:text-black outline-none focus-within:text-black'
-					onClick={handleToggle}>
+			<article className='flex justify-end align-start w-full '>
+				<button className='text-8xl text-gray-200 self-start hover:text-black' onClick={handleToggle}>
 					{isShowTestScores ? '-' : '+'}
 				</button>
 			</article>
