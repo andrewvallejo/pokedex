206ca38a0e2328276b17625101f463fc5a9ab6f0
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-02-01 11:45:30 -0700

    Add JSX for test scores

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,9 +1,21 @@
 /** components | Student
  * @description: A single student
- * @elements: id, fullName, email, company, skill, average, grades, picture(pic)
+ * @elements: id, fullName, email, company, skill, average(of grades), test scores(grades), picture(pic)
  */
 
 export const Student = ({student}) => {
+	const testScores = student.grades.map((grade, i) => {
+		const testNumber = i + 1
+		return (
+			<li
+				key={`${student.name}: test ${testNumber}`}
+				className='flex flex-row self-start justify-between w-2/5 -space-x-4'>
+				<p>Test {testNumber}</p>
+				<p>{grade}%</p>
+			</li>
+		)
+	})
+
 	return (
 		<li className='flex flex-wrap py-2 px-4 '>
 			<img className='rounded-full border-black border w-32 h-32' src={student.pic} alt={student.firstName} />
@@ -17,6 +29,7 @@ export const Student = ({student}) => {
 						Average:<span>{student.average}</span>%
 					</div>
 				</div>
+				<ul className='py-2 px-5'>{testScores}</ul>
 			</div>
 		</li>
 	)
