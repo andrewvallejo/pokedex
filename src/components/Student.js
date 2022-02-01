69fc761742320a644ca2976be215225a8e208f30
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-01-31 17:50:53 -0700

    Remove logic to keep component simple

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,17 +1,15 @@
-export const Student = ({student, average}) => {
+export const Student = ({student}) => {
 	return (
 		<li className='flex flex-wrap py-2 px-4 '>
 			<img className='rounded-full border-black border w-32 h-32' src={student.pic} alt={student.firstName} />
 			<div className='px-5'>
-				<h2 className='font-bold uppercase text-4xl '>
-					{student.firstName} {student.lastName}
-				</h2>
+				<h2 className='font-bold uppercase text-4xl '>{student.name}</h2>
 				<div className='py-2 px-5'>
 					<p>Email: {student.email}</p>
 					<p>Company: {student.company}</p>
 					<p>Skill: {student.skill}</p>
 					<div className='flex'>
-						Average:<span>{average}</span>%
+						Average:<span>{student.average}</span>%
 					</div>
 				</div>
 			</div>
