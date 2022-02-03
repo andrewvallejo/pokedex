2eb41cd0f4d2d3b2fa22375127bffc6162f1f22c
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-02-03 14:05:31 -0700

    Refactor SearchBar to be completely agnostic

--- src/features/search/SearchBar.js
+++ src/features/search/SearchBar.js
@@ -9,12 +9,12 @@
 import {useDispatch, useSelector} from 'react-redux'
 import {setSearchTerm} from './searchSlice'
 
-export const SearchBar = () => {
-	const {search: {searchTerm, searchField}} = useSelector((state) => state.reducer)
+export const SearchBar = ({field}) => {
+	const {searchTerms} = useSelector((state) => state.reducer.search)
 	const dispatch = useDispatch()
 
 	const handleChange = (e) => {
-		dispatch(setSearchTerm(e.target.value))
+		dispatch(setSearchTerm({field, value: e.target.value}))
 	}
 
 	return (
@@ -22,8 +22,8 @@ export const SearchBar = () => {
 			<input
 				className='w-full h-12 p-1 text-lg hover:placeholder-black focus:outline-none focus:ring-2 focus:placehholder-black '
 				type='text'
-				placeholder={`Search by ${searchField}`}
-				value={searchTerm}
+				placeholder={`Search by ${field}`}
+				value={searchTerms[field].value}
 				onChange={handleChange}
 			/>
 		</div>
