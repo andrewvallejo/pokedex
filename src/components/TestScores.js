467a39cfc1f7d732dbdd1b3052cf615692a3bc55
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-02-03 12:25:26 -0700

    Add specific width to list item

--- src/components/TestScores.js
+++ src/components/TestScores.js
@@ -2,7 +2,7 @@ export const TestScores = ({name, grades}) =>
 	grades.map((grade, i) => {
 		const testNumber = i + 1
 		return (
-			<li key={`${name}: test ${testNumber}`} className='flex flex-row self-start justify-between w-3/5'>
+			<li key={`${name}: test ${testNumber}`} className='flex flex-row self-start justify-between w-44'>
 				<p>Test {testNumber}:</p>
 				<p>{grade}%</p>
 			</li>
