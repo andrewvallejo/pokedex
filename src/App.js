2d1d423729f5d8f073a047e4c2c03fae432d2522
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-02-02 18:05:22 -0700

    Update import path

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import {useEffect} from 'react'
 import {useDispatch} from 'react-redux'
 import {Route, Routes} from 'react-router-dom'
 import {fetchStudents} from './api/studentApi'
-import {StudentPage} from './components/StudentPage'
+import {StudentPage} from './features/students/StudentPage'
 
 export const App = () => {
 	const dispatch = useDispatch()
