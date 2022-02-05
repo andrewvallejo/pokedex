9067478d579270d1f8e66f1d4c10347241669b4e
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-02-05 15:46:14 -0700

    Add a loader list for student placeholder

--- src/components/List.js
+++ src/components/List.js
@@ -1,8 +1,10 @@
 import {Student} from '../features/students/Student'
+import {Loader} from './Loader'
 
 export const List = ({list, type}) => {
 	const selections = {
-		students: list.map((student) => <Student key={student.id} student={student} />)
+		students: list.map((student) => <Student key={student.id} student={student} />),
+		loader: Array.from({length: 3}).map((_, i) => <Loader key={i} />)
 	}
 
 	return <ul className='divide-y divide-gray-100 overflow-auto overscroll-auto h-full snap-x '>{selections[type]}</ul>
