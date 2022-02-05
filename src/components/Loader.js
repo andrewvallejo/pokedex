cd5f3530e9b43992f2312c3d02b0dd81662b8ea4
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-02-05 15:46:29 -0700

    Refactor into a pulsing animation of the layout

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,5 +1,31 @@
-export const Loader = (
-	<div className='flex flex-col justify-center items-center'>
-		<h2 className='text-3xl font-bold'>Loading...</h2>
-	</div>
-)
+export const Loader = () => {
+	return (
+		<li className='flex flex-col  px-4 h-3/5 md:h-1/2 w-full overscroll-auto snap-bottom overflow-hidden  -mb-2'>
+			<div className='animate-pulse  flex w-full justify-center flex-wrap relative'>
+				<div className='rounded-full mt-4 bg-gray-100 w-40 h-40 md:w-32 md:h-32 border' />
+				<div>
+					<div className='px-5 flex-col  w-full  mt-6'>
+						<div className='h-8 w-full   bg-gray-100 rounded ' />
+						<div className='py-4 ml-4  pr-5 grid grid-cols-1 gap-2   '>
+							<div className=' h-2 w-full mt-2 bg-gray-100 rounded ' />
+							<div className='h-2 w-full bg-gray-100 rounded' />
+							<div className='h-2 w-full bg-gray-100 rounded' />
+							<div className='h-2 w-full bg-gray-100 rounded' />
+							<div className='py-4'>
+								<input
+									className='h-2 bg-gray-100  p-1 text-md border-b-2 border-b-gray-100'
+									type='text'
+								/>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className='flex  flex-1 flex-row-reverse '>
+					<button className='absolute -top-5 md:top-4 text-[10rem] md:text-8xl text-gray-200 hover:text-black'>
+						+
+					</button>
+				</div>
+			</div>
+		</li>
+	)
+}
