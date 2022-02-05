1663ea0955d52f19abd832f273b0a4675ed65b94
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-02-05 10:52:11 -0700

    Fix button mobile top placement

--- src/features/students/Student.js
+++ src/features/students/Student.js
@@ -83,7 +83,7 @@ export const Student = ({student}) => {
 				</div>
 				<div className='flex  flex-1 flex-row-reverse'>
 					<button
-						className='absolute top-5  md:top-0 text-[10rem] md:text-8xl text-gray-200 hover:text-black'
+						className='absolute -top-10 md:top-0 text-[10rem] md:text-8xl text-gray-200 hover:text-black'
 						onClick={handleToggle}>
 						{isShowTestScores ? '-' : '+'}
 					</button>
