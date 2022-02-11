9baf70528521ee59c7dded7a7c5f2365758cc802
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-02-11 08:28:53 -0700

    Add error handling for no students

--- src/components/List.js
+++ src/components/List.js
@@ -8,8 +8,13 @@ export const List = ({list, type}) => {
 	}
 
 	return (
-		<ul className='divide-y divide-gray-100 overscroll-auto h-full snap-x' data-cy={`list-${type}`}>
+		<ul className='divide-y divide-gray-100 overscroll-auto h-full snap-x relative' data-cy={`list-${type}`}>
 			{selections[type]}
+			{!list.length && (
+				<p className='text-center text-5xl text-gray-500 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+					No {type} found
+				</p>
+			)}
 		</ul>
 	)
 }
