9b600f8d7432ab81395010174fd5f86ad6b1fda0
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-02-19 11:34:30 -0700

    Add pokemonApi reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,17 +1,18 @@
 import {combineReducers, configureStore} from '@reduxjs/toolkit'
 import {setupListeners} from '@reduxjs/toolkit/query'
-import searchReducer from './features/search/searchSlice'
 import pokemonReducer from './features/pokemon/pokemonSlice'
+import searchReducer from './features/search/searchSlice'
+import {pokemonApi} from './services/pokemon'
 
 const rootReducer = combineReducers({
+	[pokemonApi.reducerPath]: pokemonApi.reducer,
 	pokemon: pokemonReducer,
 	search: searchReducer
 })
 
 export const store = configureStore({
-	reducer: {
-		reducer: rootReducer
-	}
+	reducer: rootReducer,
+	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(pokemonApi.middleware)
 })
 
 setupListeners(store.dispatch)
