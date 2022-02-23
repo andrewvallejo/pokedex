bb2394c3c328d61151e0490a3600a61d033a73b5
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-02-23 13:28:40 -0700

    Add keyframes for scroll animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,8 @@ module.exports = {
 				iii: 'hsl(9, 52%, 65%)',
 				iiii: 'hsl(341, 35%, 43%)',
 				iiiii: 'hsl(48, 93%, 63%)',
-				iiiiii: 'hsl(207, 83%, 61%)'
+				iiiiii: 'hsl(207, 83%, 61%)',
+				iiiiiii: 'hsl(87, 90%, %)'
 			},
 			red: {
 				i: 'hsl(352, 85%, 57%)',
@@ -25,6 +26,27 @@ module.exports = {
 				200: '#a0a0a0'
 			},
 			transparent: 'transparent'
+		},
+		extend: {
+			keyframes: {
+				scroll: {
+					'0%': {
+						transform: 'translateX(100%)'
+					},
+					'100%': {
+						transform: 'translateX(-100%)'
+					}
+				},
+				opi: {
+					'50%': {
+						opacity: '.75'
+					}
+				}
+			},
+			animation: {
+				scroll: 'scroll 10s infinite linear',
+				opi: 'opi 3s infinite alternate'
+			}
 		}
 	}
 }
