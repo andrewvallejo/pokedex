3ec5fe512f34e7ea313de846bef17288ebcbb47e
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-02-24 08:39:21 -0700

    Fix spacing within controller

--- src/components/pokedex/FakeController.js
+++ src/components/pokedex/FakeController.js
@@ -1,12 +1,12 @@
 export const FakeController = () => {
 	return (
 		<aside className='flex flex-col fixed h-full w-3/4 lg:w-full scale-[0.85] py-10 rounded-tr-[10rem]  justify-around'>
-			<div className='p-10  h-4/5 w-2/5 rounded-tr-[10rem] bg-clr-iiii border-4 rounded border-b-red-i b
-	order-l-red-i border-t-red-iii border-r-red-iii border-red-i shadow-md absolute self-center  overflow-hidden'>
-				<div className='flex flex-col rounded-3xl'>
-					<header className='z-10 flex-row-reverse items-center justify-around hidden mt-4 md:flex rounded-3xl'>
+			<div className='p-4  h-4/5 w-2/5 rounded-tr-[10rem]  bg-clr-iiii border-4 rounded border-b-red-i b
+	order-l-red-i border-t-red-iii border-r-red-iii border-red-i shadow-md absolute self-center overflow'>
+				<div className='flex flex-col justify-around h-1/3 rounded-3xl'>
+					<header className='z-10 flex-row-reverse items-center justify-around hidden md:flex rounded-3xl'>
 						<div className='flex flex-col-reverse flex-wrap md:flex-row'>
-							<h1 className='px-10 py-4 my-10 mb-10 text-center border-2 l:ml-10 md:text-4xl bg-red-ii rounded-3xl border-clr-iiiii text-clr-iiiii'>
+							<h1 className='px-10 py-4 text-center border-2 md:block l:ml-10 md:text-4xl bg-red-ii rounded-3xl border-clr-iiiii text-clr-iiiii'>
 								Poke
 								<span className='text-clr-'>dex</span>
 							</h1>
@@ -35,7 +35,7 @@ export const FakeController = () => {
 					</div>
 				</div>
 				<article className='flex-col justify-around hidden w-full px-8 py-5 bg-gray-100 rounded md:flex drop-shadow-md h-3/5 '>
-					<div className='flex flex-col w-full h-full pt-10 mb-4 overflow-scroll text-lg bg-white border-2 rounded shadow justify-evenly'>
+					<div className='flex flex-col mb-4 overflow-auto text-lg bg-white border-2 rounded shadow w-fullpx-2 justify-evenly'>
 						<h3 className='text-3xl text-center'>How to use</h3>
 						<p>
 							<span className='text-2xl font-bold text-red-i'>»</span> Scroll through the Pokedex to learn
