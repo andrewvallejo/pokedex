75917809dc3332db42d9d0df668f1b1025014d23
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-02-24 09:59:55 -0700

    Refactor list to handle stats

--- src/components/pokedex/List.js
+++ src/components/pokedex/List.js
@@ -1,15 +1,29 @@
+import {useEffect, useState} from 'react'
 import {Pokemon} from '../../app/features/pokemon/Pokemon'
+import {Stat} from './Stat'
 
 export const List = ({list, type}) => {
+	const [stats, setStats] = useState([])
+
+	useEffect(
+		() => {
+			if (type === 'stats') {
+				setStats(list[1])
+			}
+		},
+		[list, type]
+	)
+
 	const selections = {
-		pokemon: list.map((pokemon) => <Pokemon key={pokemon.id} pokemon={pokemon} />)
+		pokemon: list.map((pokemon) => <Pokemon key={pokemon.id} pokemon={pokemon} />),
+		stats: stats.map((stat) => <Stat key={`${list[0]}-${stat.name}`} stat={stat} />)
 	}
 
 	return (
 		<ul
 			className='relative z-10 min-h-full overflow-y-scroll divide-y divide-red-i snap-x'
 			data-cy={`list-${type}`}>
-			{selections[type]}
+			{list && selections[type]}
 			{!list.length && (
 				<p className='absolute text-5xl text-center text-gray-500 transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'>
 					No pokemon found
