d6616ea05fee9f8584f93cc8774ab43d92603643
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-02-23 20:45:36 -0700

    Update details to be responsive

--- src/components/pokedex/FakeController.js
+++ src/components/pokedex/FakeController.js
@@ -1,39 +1,40 @@
 export const FakeController = () => {
 	return (
 		<aside className='flex flex-col fixed h-full w-3/4 lg:w-full scale-[0.85] py-10 rounded-tr-[10rem]  justify-around'>
-			<div className='p-10 h-4/5 w-2/5 rounded-tr-[10rem] bg-clr-iiii border-4 rounded border-b-red-i b
-	order-l-red-i border-t-red-iii border-r-red-iii border-red-i shadow-md absolute self-center  '>
+			<div className='p-10  h-4/5 w-2/5 rounded-tr-[10rem] bg-clr-iiii border-4 rounded border-b-red-i b
+	order-l-red-i border-t-red-iii border-r-red-iii border-red-i shadow-md absolute self-center  overflow-hidden'>
 				<div className='flex flex-col rounded-3xl'>
-					<header className='z-10 flex-row-reverse items-center justify-around hidden xl:flex rounded-3xl'>
-						<div>
-							<div className='w-16 h-16 mx-10 border-4 border-gray-100 rounded-full bg-clr-iiiiii' />
-						</div>
-
-						<h1 className='px-10 py-4 mx-2 text-center border-2 xl:text-4xl bg-red-ii rounded-3xl border-clr-iiiii text-clr-iiiii'>
-							Poke
-							<span className='text-clr-'>dex</span>
-						</h1>
-						<div className='flex w-1/3 justify-evenly'>
-							<div className='w-10 h-10 border-2 border-black rounded-full bg-clr-iiiiii ' />
-							<div className='w-10 h-10 border-2 border-black rounded-full bg-clr-iiiii' />
-							<div className='w-10 h-10 border-2 border-black rounded-full bg-red-i' />
+					<header className='z-10 flex-row-reverse items-center justify-around hidden mt-4 md:flex rounded-3xl'>
+						<div className='flex flex-col-reverse flex-wrap md:flex-row'>
+							<h1 className='px-10 py-4 my-10 mb-10 text-center border-2 l:ml-10 md:text-4xl bg-red-ii rounded-3xl border-clr-iiiii text-clr-iiiii'>
+								Poke
+								<span className='text-clr-'>dex</span>
+							</h1>
+							<div className='flex flex-row-reverse items-center justify-around mx-3 mb-2 l:flex-row'>
+								<div className='w-10 h-10 mr-1 border-2 border-black rounded-full bg-clr-iiiiii ' />
+								<div className='w-10 h-10 mr-1 border-2 border-black rounded-full bg-clr-iiiii' />
+								<div className='w-10 h-10 mr-1 border-2 border-black rounded-full bg-red-i' />
+								<div>
+									<div className='border-4 border-gray-100 rounded-full w-14 h-14 l:h-20 l:w-20 l:mx-10 bg-clr-iiiiii' />
+								</div>
+							</div>
 						</div>
 					</header>
-					<div className='hidden w-full h-20 mt-10 mb-4 overflow-hidden border-2 shadow-md xl:flex bg-clr-iiiiii rounded-xl border-clr-ii '>
+					<div className='hidden w-full h-20 my-4 overflow-hidden border-2 shadow-md md:flex bg-clr-iiiiii rounded-xl border-clr-ii '>
 						<div className='flex w-full divide-x animate-scroll divide-clr-ii '>
-							<div className='w-full ' />
-							<div className='w-full ' />
-							<div className='w-full ' />
-							<div className='w-full ' />
-							<div className='w-full ' />
-							<div className='w-full ' />
-							<div className='w-full ' />
-							<div className='w-full ' />
-							<div className='w-full ' />
+							<div className='w-full' />
+							<div className='w-full' />
+							<div className='w-full' />
+							<div className='w-full' />
+							<div className='w-full' />
+							<div className='w-full' />
+							<div className='w-full' />
+							<div className='w-full' />
+							<div className='w-full' />
 						</div>
 					</div>
 				</div>
-				<article className='flex-col justify-around hidden w-full px-8 py-5 bg-gray-100 rounded xl:flex drop-shadow-md h-3/5'>
+				<article className='flex-col justify-around hidden w-full px-8 py-5 bg-gray-100 rounded md:flex drop-shadow-md h-3/5 '>
 					<div className='flex flex-col w-full h-full pt-10 mb-4 overflow-scroll text-lg bg-white border-2 rounded shadow justify-evenly'>
 						<h3 className='text-3xl text-center'>How to use</h3>
 						<p>
